edb6f9e8c00b41f1ba4a765ae280558928fcaf85
Rizq Hilal Rifaasya <98735640+rzhilal@users.noreply.github.com>
2022-12-20 21:50:53 +0700

    Game play sederhana

--- Script/Playscene/lasser.gd
+++ Script/Playscene/lasser.gd
@@ -1,13 +1,20 @@
 extends KinematicBody
 
 var velo = Vector3()
+var KillParticles = load("res://Scene/Play Scene/KillParticles.tscn")
+onready var main = get_tree().current_scene
+onready var explodeSound = $explode
 
 func _physics_process(delta):
 	move_and_slide(velo)
 
 
-func _on_Area_body_entered(body):
+func _on_Area_body_entered(body):	
 	if body.is_in_group("enemies"):
+		var particles = KillParticles.instance()
+		main.add_child(particles)
+		particles.transform.origin = transform.origin
 		body.queue_free()
+		explodeSound.play()
 		visible = false
 		$Area/CollisionShape.disabled = true
