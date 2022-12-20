9241ef001ad82140d9aaca9063983dbee2802fe5
Rizq Hilal Rifaasya <98735640+rzhilal@users.noreply.github.com>
2022-12-20 21:17:37 +0700

    Interact object

--- Script/Playscene/enemy.gd
+++ Script/Playscene/enemy.gd
@@ -1,13 +1,16 @@
 extends KinematicBody
 
-var velo = Vector3()
+
+var spd = rand_range(20,50)
 
 func _physics_process(delta):
-	move_and_slide(velo)
+	move_and_slide(Vector3(0,0,spd))
+	if transform.origin.z > 10:
+		queue_free()
 
 
 func _on_Area_body_entered(body):
-	if body.is_in_group("enemies"):
+	if body.is_in_group("avatarbody"):
 		body.queue_free()
 		visible = false
 		$Area/CollisionShape.disabled = true
