fe7d9fc6a701633592e04648552bf65a482d1041
Hazumiran <diazazhari32@gmail.com>
2022-12-21 16:31:39 +0700

    penambahan ui dan penyatuan page

---
+++ Script/Menu/mainmenu.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_ExitButton_pressed():
+		get_tree().quit()
+
+
+func _on_AboutButton_pressed():
+	get_tree().change_scene("res://Scene/About/About.tscn")
+
+
+func _on_PlayButton_pressed():
+	get_tree().change_scene("res://Scene/Play Scene/main.tscn")
+
+
+func _on_HelpButton_pressed():
+	get_tree().change_scene("res://Scene/Help/Help.tscn")
