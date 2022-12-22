077368288957a05de37f7fd7971d0cd8917b7df2
Rizq Hilal Rifaasya <98735640+rzhilal@users.noreply.github.com>
2022-12-22 10:27:12 +0700

    Highscore system

--- Script/Globals.gd
+++ Script/Globals.gd
@@ -4,12 +4,15 @@ var score = 0
 var life = 3
 var power = false
 var level_spawn = 0.8
+var highscore = 0
 
 var spacheship1_isequipped = true
 var spacheship2_isequipped = false
 var spacheship3_isequipped = false
 
 func reset():
+	if(score > highscore):
+		highscore = score
 	score = 0
 	life = 3
 	power = false
